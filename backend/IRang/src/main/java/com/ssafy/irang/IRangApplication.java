590e67b309e9bdeb9400a5ee8ed561546fc3585d
강인혁 <ggh2260@gmail.com>
2025-01-17 16:55:27 +0900

    Chore: 스프링부트 백엔드 프로젝트 초기 세팅

---
+++ backend/IRang/src/main/java/com/ssafy/irang/IRangApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.irang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IRangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IRangApplication.class, args);
+    }
+
+}
---
+++ backend/IRang/src/test/java/com/ssafy/irang/IRangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.irang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IRangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
