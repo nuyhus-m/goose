d27af938e26a53202c7bfc6a3293e399fb878b89
nuyhus-m <minsh2344@gmail.com>
2025-01-27 19:15:19 +0900

    Chore: 네트워크 관련 구조 설정

---
+++ android/app/src/main/java/com/ssafy/firstproject/data/model/User.kt
@@ -0,0 +1,5 @@
+package com.ssafy.firstproject.data.model
+
+data class User(
+    val id: Int,
+)
--- android/app/src/main/java/com/ssafy/firstproject/data/remote/RetrofitUtil.kt
+++ android/app/src/main/java/com/ssafy/firstproject/data/remote/RetrofitUtil.kt
@@ -1,7 +1,9 @@
 package com.ssafy.firstproject.data.remote
 
+import com.ssafy.firstproject.base.ApplicationClass
+
 class RetrofitUtil {
     companion object {
-
+        val customerService: UserService = ApplicationClass.retrofit.create(UserService::class.java)
     }
 }
