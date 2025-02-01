bbf2c8cd565b76b1fc07e4d1c803e94a0f5a7a91
nuyhus-m <minsh2344@gmail.com>
2025-02-02 01:34:45 +0900

    Feat: 게임시작화면에서 게임화면으로 이동 구현

--- android/app/src/main/java/com/ssafy/firstproject/ui/game/GameStartFragment.kt
+++ android/app/src/main/java/com/ssafy/firstproject/ui/game/GameStartFragment.kt
@@ -1,5 +1,8 @@
 package com.ssafy.firstproject.ui.game
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.ssafy.firstproject.R
 import com.ssafy.firstproject.base.BaseFragment
 import com.ssafy.firstproject.databinding.FragmentGameStartBinding
@@ -10,4 +13,12 @@ class GameStartFragment : BaseFragment<FragmentGameStartBinding>(
     FragmentGameStartBinding::bind,
     R.layout.fragment_game_start
 ) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btn.setOnClickListener {
+            findNavController().navigate(R.id.dest_game)
+        }
+    }
 }
