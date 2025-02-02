84011ec15b6a181460309ec3dcaf80f28e5c9c24
민수현 <minsh2344@gmail.com>
2025-02-02 19:43:16 +0900

    Feat: 게임결과화면 프래그먼트 구현

---
+++ android/app/src/main/java/com/ssafy/firstproject/ui/gameresult/GameResultFragment.kt
@@ -0,0 +1,13 @@
+package com.ssafy.firstproject.ui.gameresult
+
+import com.ssafy.firstproject.R
+import com.ssafy.firstproject.base.BaseFragment
+import com.ssafy.firstproject.databinding.FragmentGameResultBinding
+
+private const val TAG = "GameResultFragment"
+
+class GameResultFragment : BaseFragment<FragmentGameResultBinding>(
+    FragmentGameResultBinding::bind,
+    R.layout.fragment_game_result
+) {
+}
