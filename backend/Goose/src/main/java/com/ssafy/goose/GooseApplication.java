aae11c5978a553a1ba20e8bb59871736ad6fee35
강인혁 <ggh2260@gmail.com>
2025-02-02 18:59:09 +0900

    Feat: 6시간 마다 실시간 인기검색어 추출, 뉴스 검색 후 mongoDB 저장 기능 구현 Related to: #S12P11D208-74
    Former-commit-id: 9830d15e948eebd89ecd73004be23a4748e29d5f

--- backend/Goose/src/main/java/com/ssafy/goose/GooseApplication.java
+++ backend/Goose/src/main/java/com/ssafy/goose/GooseApplication.java
@@ -1,7 +1,10 @@
 package com.ssafy.goose;
 
+import com.ssafy.goose.domain.news.crawling.service.AutoCrawlingService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -12,4 +15,12 @@ public class GooseApplication {
 		SpringApplication.run(GooseApplication.class, args);
 	}
 
+	// 🔹 앱 시작 후 즉시 뉴스 크롤링 실행
+	@Bean
+	public CommandLineRunner run(AutoCrawlingService autoCrawlingService) {
+		return args -> {
+			System.out.println("🚀 애플리케이션 실행 후 즉시 뉴스 크롤링 시작...");
+			autoCrawlingService.fetchAndSaveTrendingNews(); // 즉시 실행
+		};
+	}
 }
