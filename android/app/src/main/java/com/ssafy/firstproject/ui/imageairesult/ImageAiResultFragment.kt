0dd3d09796aa40ddcc0b998678c117841661164b
yangdahye <yangdahye.e@daum.net>
2025-02-03 17:03:14 +0900

    Feat: 이미지 판별 결과 화면 프래그먼트 등 구현

---
+++ android/app/src/main/java/com/ssafy/firstproject/ui/imageairesult/ImageAiResultFragment.kt
@@ -0,0 +1,11 @@
+package com.ssafy.firstproject.ui.imageairesult
+
+import com.ssafy.firstproject.R
+import com.ssafy.firstproject.base.BaseFragment
+import com.ssafy.firstproject.databinding.FragmentImageAiResultBinding
+
+class ImageAiResultFragment : BaseFragment<FragmentImageAiResultBinding>(
+    FragmentImageAiResultBinding::bind,
+    R.layout.fragment_image_ai_result
+) {
+}
