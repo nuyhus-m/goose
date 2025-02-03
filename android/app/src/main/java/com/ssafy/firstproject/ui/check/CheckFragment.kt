f559b894e853bab3d13d617966c5a05d0c5ccf81
gretea5 <gretea4489@gmail.com>
2025-02-03 09:26:30 +0900

    Fix: dropdown item 리스트가 유지되지 않는 문제 해결

--- android/app/src/main/java/com/ssafy/firstproject/ui/check/CheckFragment.kt
+++ android/app/src/main/java/com/ssafy/firstproject/ui/check/CheckFragment.kt
@@ -1,13 +1,32 @@
 package com.ssafy.firstproject.ui.check
 
+import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
 import com.ssafy.firstproject.R
 import com.ssafy.firstproject.base.BaseFragment
 import com.ssafy.firstproject.databinding.FragmentCheckBinding
 
-private const val TAG = "CheckFragment"
-
+private const val TAG = "CheckFragment_ssafy"
 class CheckFragment : BaseFragment<FragmentCheckBinding>(
     FragmentCheckBinding::bind,
     R.layout.fragment_check
 ) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val typeList = resources.getStringArray(R.array.check_types)
+
+        val adapter: ArrayAdapter<String> = ArrayAdapter<String>(
+            requireContext(),
+            android.R.layout.simple_dropdown_item_1line,
+            typeList
+        )
+
+        binding.autoCompleteTextView.setAdapter(adapter)
+    }
 }
