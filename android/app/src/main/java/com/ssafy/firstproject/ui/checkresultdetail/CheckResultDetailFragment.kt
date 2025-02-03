dd28c94acabfc37b4bfc3520ddacd2f6de6fa161
gretea5 <gretea4489@gmail.com>
2025-02-03 15:00:21 +0900

    Remove: 로깅 import 삭제

--- android/app/src/main/java/com/ssafy/firstproject/ui/checkresultdetail/CheckResultDetailFragment.kt
+++ android/app/src/main/java/com/ssafy/firstproject/ui/checkresultdetail/CheckResultDetailFragment.kt
@@ -1,7 +1,5 @@
 package com.ssafy.firstproject.ui.checkresultdetail
 
-import android.os.Bundle
-import android.view.View
 import com.ssafy.firstproject.R
 import com.ssafy.firstproject.base.BaseFragment
 import com.ssafy.firstproject.databinding.FragmentCheckResultDetailBinding
