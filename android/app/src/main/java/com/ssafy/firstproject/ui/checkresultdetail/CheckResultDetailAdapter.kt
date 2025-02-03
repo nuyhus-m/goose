6fb15069c28909be00c7050e83f4f8fd01040514
gretea5 <gretea4489@gmail.com>
2025-02-03 17:06:03 +0900

    Feat: 뉴스 문단 RecyclerView 세팅

---
+++ android/app/src/main/java/com/ssafy/firstproject/ui/checkresultdetail/CheckResultDetailAdapter.kt
@@ -0,0 +1,33 @@
+package com.ssafy.firstproject.ui.checkresultdetail
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.ssafy.firstproject.data.model.NewsContent
+import com.ssafy.firstproject.databinding.ItemNewsContentBinding
+
+class CheckResultDetailAdapter : ListAdapter<NewsContent, CheckResultDetailAdapter.CheckResultDetailViewHolder>(CheckNewsDiffCallback()) {
+    inner class CheckResultDetailViewHolder(private val binding: ItemNewsContentBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(newsContent: NewsContent) {
+            binding.tvNewsItemContent.text = newsContent.content
+            binding.tvResult.text = newsContent.result
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CheckResultDetailViewHolder {
+        val binding = ItemNewsContentBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+
+        return CheckResultDetailViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CheckResultDetailAdapter.CheckResultDetailViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
