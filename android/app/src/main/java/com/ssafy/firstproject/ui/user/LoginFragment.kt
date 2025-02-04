1bb581ca886af704701756546afd714e1578d1cd
gretea5 <gretea4489@gmail.com>
2025-02-04 11:25:53 +0900

    Feat: 로그인 Fragment 생성

---
+++ android/app/src/main/java/com/ssafy/firstproject/ui/user/LoginFragment.kt
@@ -0,0 +1,12 @@
+package com.ssafy.firstproject.ui.user
+
+import com.ssafy.firstproject.R
+import com.ssafy.firstproject.base.BaseFragment
+import com.ssafy.firstproject.databinding.FragmentLoginBinding
+
+class LoginFragment : BaseFragment<FragmentLoginBinding>(
+    FragmentLoginBinding::bind,
+    R.layout.fragment_login
+){
+
+}
