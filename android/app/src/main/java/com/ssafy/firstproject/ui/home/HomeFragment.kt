fb377827317c30dac16d8e60818767e8d038a6de
민수현 <minsh2344@gmail.com>
2025-02-06 01:39:57 +0900

    Feat: 홈 화면 뉴스 리스트 구현
    Former-commit-id: 55eeecf8b5715632371e563d65763c123398a6a2

--- android/app/src/main/java/com/ssafy/firstproject/ui/home/HomeFragment.kt
+++ android/app/src/main/java/com/ssafy/firstproject/ui/home/HomeFragment.kt
@@ -1,8 +1,14 @@
 package com.ssafy.firstproject.ui.home
 
+import android.graphics.drawable.AnimationDrawable
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
 import com.ssafy.firstproject.R
 import com.ssafy.firstproject.base.BaseFragment
 import com.ssafy.firstproject.databinding.FragmentHomeBinding
+import com.ssafy.firstproject.ui.home.adpater.NewsAdapter
+import com.ssafy.firstproject.ui.home.viewmodel.HomeViewModel
 
 private const val TAG = "HomeFragment"
 
@@ -10,4 +16,24 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(
     FragmentHomeBinding::bind,
     R.layout.fragment_home
 ) {
+    private lateinit var adapter: NewsAdapter
+    private val viewModel by viewModels<HomeViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initAdapter()
+        observeNewsList()
+    }
+
+    private fun initAdapter() {
+        adapter = NewsAdapter {}
+        binding.rvNews.adapter = adapter
+    }
+
+    private fun observeNewsList() {
+        viewModel.newsList.observe(viewLifecycleOwner) {
+            adapter.submitList(it)
+        }
+    }
 }
