95a3e3959df8fd5bb6ed4ff9eb292fb8dccce0bb
gretea5 <gretea4489@gmail.com>
2025-02-05 16:10:03 +0900

    Feat: 회원가입 Fragment 생성

---
+++ android/app/src/main/java/com/ssafy/firstproject/ui/user/SignupFragment.kt
@@ -0,0 +1,12 @@
+package com.ssafy.firstproject.ui.user
+
+import com.ssafy.firstproject.R
+import com.ssafy.firstproject.base.BaseFragment
+import com.ssafy.firstproject.databinding.FragmentSignupBinding
+
+
+class SignupFragment : BaseFragment<FragmentSignupBinding>(
+    FragmentSignupBinding::bind,
+    R.layout.fragment_signup
+) {
+}
