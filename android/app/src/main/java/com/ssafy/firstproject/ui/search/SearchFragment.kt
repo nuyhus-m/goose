0946c511b2d452e6d436115337834031b801131d
민수현 <minsh2344@gmail.com>
2025-02-06 17:11:23 +0900

    Feat: 검색 화면 UI 구현
    Former-commit-id: 5029eaa9f0cfb765e8d2ec0832e9083299bc0f53

---
+++ android/app/src/main/java/com/ssafy/firstproject/ui/search/SearchFragment.kt
@@ -0,0 +1,11 @@
+package com.ssafy.firstproject.ui.search
+
+import com.ssafy.firstproject.R
+import com.ssafy.firstproject.base.BaseFragment
+import com.ssafy.firstproject.databinding.FragmentSearchBinding
+
+class SearchFragment : BaseFragment<FragmentSearchBinding>(
+    FragmentSearchBinding::bind,
+    R.layout.fragment_search
+) {
+}
