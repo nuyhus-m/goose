61c36a5dcfe0e5597b5f14f1e57409447829edc4
gretea5 <gretea4489@gmail.com>
2025-02-07 13:34:26 +0900

    Feat: 아이디, 비밀번호 입력 메시지 처리
    Former-commit-id: d2a99f5c1089b2003fc6791162ed5a57aa7eba4d

--- android/app/src/main/java/com/ssafy/firstproject/ui/signup/SignupFragment.kt
+++ android/app/src/main/java/com/ssafy/firstproject/ui/signup/SignupFragment.kt
@@ -2,11 +2,15 @@ package com.ssafy.firstproject.ui.signup
 
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.textfield.TextInputEditText
 import com.ssafy.firstproject.R
 import com.ssafy.firstproject.base.BaseFragment
 import com.ssafy.firstproject.databinding.FragmentSignupBinding
 
+private const val TAG = "SignupFragment_ssafy"
 class SignupFragment : BaseFragment<FragmentSignupBinding>(
     FragmentSignupBinding::bind,
     R.layout.fragment_signup
@@ -15,5 +19,28 @@ class SignupFragment : BaseFragment<FragmentSignupBinding>(
         super.onViewCreated(view, savedInstanceState)
 
         binding.ivSignupClose.setOnClickListener { findNavController().popBackStack() }
+
+        binding.tieSignupIdInput.addTextChangedListener {
+            validateInput(binding.tieSignupIdInput, binding.tvIdInputMetaInfo)
+        }
+
+        binding.tieSignupPwInput.addTextChangedListener {
+            validateInput(binding.tieSignupPwInput, binding.tvPwMetaInfo)
+        }
+
+        binding.tieSignupNicknameInput.addTextChangedListener {
+            validateInput(binding.tieSignupNicknameInput, binding.tvNicknameMetaInfo)
+        }
+    }
+
+    private fun validateInput(editText: TextInputEditText, textView: TextView) {
+        val text = editText.text.toString().trim()
+        val pattern = "^[a-z0-9]{4,12}$".toRegex()
+
+        when {
+            text.isEmpty() -> { textView.visibility = View.GONE }
+            !pattern.matches(text) -> { textView.visibility = View.VISIBLE }
+            else -> { textView.visibility = View.GONE }
+        }
     }
 }
