1c016766340be3a721e173bb4ce6b2d9bbe4944e
민수현 <minsh2344@gmail.com>
2025-02-09 18:38:29 +0900

    Feat: 검색 api 연동
    Former-commit-id: 2a61ed67579f2d8521e424be38043bf97bd24e99

--- android/app/src/main/java/com/ssafy/firstproject/data/repository/NewsRepository.kt
+++ android/app/src/main/java/com/ssafy/firstproject/data/repository/NewsRepository.kt
@@ -1,13 +1,17 @@
 package com.ssafy.firstproject.data.repository
 
-import com.ssafy.firstproject.data.model.News
+import com.ssafy.firstproject.data.model.NewsArticle
 import com.ssafy.firstproject.data.source.remote.RetrofitUtil
 
 class NewsRepository {
 
     private val newsService = RetrofitUtil.newsService
 
-    suspend fun getNewsList(): List<News> {
+    suspend fun getNewsList(): List<NewsArticle> {
         return newsService.getNewsList()
+    }
+
+    suspend fun getSearchNewsList(keyword: String): List<NewsArticle> {
+        return newsService.getSearchNewsList(keyword)
     }
 }
--- android/app/src/main/java/com/ssafy/firstproject/data/source/remote/NewsService.kt
+++ android/app/src/main/java/com/ssafy/firstproject/data/source/remote/NewsService.kt
@@ -1,10 +1,16 @@
 package com.ssafy.firstproject.data.source.remote
 
-import com.ssafy.firstproject.data.model.News
+import com.ssafy.firstproject.data.model.NewsArticle
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface NewsService {
 
     @GET("news/list")
-    suspend fun getNewsList(): List<News>
+    suspend fun getNewsList(): List<NewsArticle>
+
+    @GET("news/search")
+    suspend fun getSearchNewsList(
+        @Query("keyword") keyword: String
+    ): List<NewsArticle>
 }
