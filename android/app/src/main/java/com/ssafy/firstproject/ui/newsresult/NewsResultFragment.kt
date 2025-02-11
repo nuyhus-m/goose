90faa6c2d04bb21f4637f20ad24d605925c1f0bd
gretea5 <gretea4489@gmail.com>
2025-02-11 23:27:32 +0900

    Feat: 뉴스 분석 결과 Fragment 생성
    Former-commit-id: bdd5be56c41ad81f29830091a55a52e8022b29dd

---
+++ android/app/src/main/java/com/ssafy/firstproject/ui/newsresult/NewsResultFragment.kt
@@ -0,0 +1,10 @@
+package com.ssafy.firstproject.ui.newsresult
+
+import com.ssafy.firstproject.R
+import com.ssafy.firstproject.base.BaseFragment
+import com.ssafy.firstproject.databinding.FragmentNewsResultBinding
+
+class NewsResultFragment : BaseFragment<FragmentNewsResultBinding>(
+    FragmentNewsResultBinding::bind,
+    R.layout.fragment_news_result
+) {}
