16b00135a985571ce7847cfea85360e9ae6214a4
민수현 <minsh2344@gmail.com>
2025-02-12 01:50:41 +0900

    Feat: 게임결과화면 UI 구현
    Former-commit-id: 180b187741fa0aabf055393ef759f6d6cb50caea

---
+++ android/app/src/main/java/com/ssafy/firstproject/ui/gameresult/GameResultFragment.kt
@@ -0,0 +1,33 @@
+package com.ssafy.firstproject.ui.gameresult
+
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.navArgs
+import com.ssafy.firstproject.R
+import com.ssafy.firstproject.base.BaseFragment
+import com.ssafy.firstproject.databinding.FragmentGameResultBinding
+
+private const val TAG = "GameResultFragment"
+
+class GameResultFragment : BaseFragment<FragmentGameResultBinding>(
+    FragmentGameResultBinding::bind,
+    R.layout.fragment_game_result
+) {
+
+    private val args: GameResultFragmentArgs by navArgs()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setData()
+    }
+
+    private fun setData() {
+        binding.tvTime.text =
+            getString(R.string.total_time_spent, millisecondsToSeconds(args.totalTimeSpent))
+    }
+
+    private fun millisecondsToSeconds(milliseconds: Long): Double {
+        return milliseconds / 1000.0
+    }
+}
