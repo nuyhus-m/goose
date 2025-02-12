c43920ad2a294cb3931300cec66213f07119e360
gretea5 <gretea4489@gmail.com>
2025-02-12 23:12:16 +0900

    Remove: 유사 뉴스 보기 클릭 로직 삭제
    Former-commit-id: ff006552fef31e50fc71aea42967fa67216de213

--- android/app/src/main/java/com/ssafy/firstproject/ui/newslistresult/NewsListResultFragment.kt
+++ android/app/src/main/java/com/ssafy/firstproject/ui/newslistresult/NewsListResultFragment.kt
@@ -6,7 +6,6 @@ import androidx.navigation.fragment.findNavController
 import com.ssafy.firstproject.R
 import com.ssafy.firstproject.base.BaseFragment
 import com.ssafy.firstproject.databinding.FragmentNewsListResultBinding
-import com.ssafy.firstproject.ui.home.HomeFragmentDirections
 import com.ssafy.firstproject.ui.home.adpater.NewsAdapter
 
 class NewsListResultFragment : BaseFragment<FragmentNewsListResultBinding>(
@@ -27,10 +26,7 @@ class NewsListResultFragment : BaseFragment<FragmentNewsListResultBinding>(
     }
 
     private fun initAdapter() {
-        adapter = NewsAdapter { id ->
-            val action = HomeFragmentDirections.actionDestHomeToDestNewsDetail(id)
-            findNavController().navigate(action)
-        }
+        adapter = NewsAdapter { id -> }
 
         binding.rvNewsResult.adapter = adapter
     }
