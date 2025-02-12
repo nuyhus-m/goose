a758e01d8bb8f98391fca6fbfe7cda46ce781210
gretea5 <gretea4489@gmail.com>
2025-02-13 01:46:06 +0900

    Refactor: ProgressBar 애니메이션 Util 클래스 생성
    Former-commit-id: 06ea7982e4b9ce2380154e82d29f3a236447a448

--- android/app/src/main/java/com/ssafy/firstproject/ui/newsresult/NewsResultFragment.kt
+++ android/app/src/main/java/com/ssafy/firstproject/ui/newsresult/NewsResultFragment.kt
@@ -1,14 +1,12 @@
 package com.ssafy.firstproject.ui.newsresult
 
-import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.View
-import android.view.animation.DecelerateInterpolator
-import android.widget.ProgressBar
 import androidx.navigation.fragment.findNavController
 import com.ssafy.firstproject.R
 import com.ssafy.firstproject.base.BaseFragment
 import com.ssafy.firstproject.databinding.FragmentNewsResultBinding
+import com.ssafy.firstproject.util.ViewAnimationUtil.animateProgress
 
 class NewsResultFragment : BaseFragment<FragmentNewsResultBinding>(
     FragmentNewsResultBinding::bind,
@@ -30,19 +28,5 @@ class NewsResultFragment : BaseFragment<FragmentNewsResultBinding>(
         animateProgress(binding.pbTruth, 30)
         animateProgress(binding.pbAi, 60)
         animateProgress(binding.pbBias, 90)
-    }
-
-    private fun animateProgress(progressBar: ProgressBar, targetProgress: Int) {
-        val currentProgress = progressBar.progress
-
-        if (targetProgress <= currentProgress) return
-
-        // 애니메이션 설정
-        val animator = ObjectAnimator.ofInt(progressBar, "progress", currentProgress, targetProgress).apply {
-            duration = 500
-            interpolator = DecelerateInterpolator()
-        }
-
-        animator.start()
     }
 }
