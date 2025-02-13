30514a48a5591b4a5b9bd0cd088e2eb0937c0553
gretea5 <gretea4489@gmail.com>
2025-02-13 23:10:39 +0900

    Feat: 문단별 판단 근거 UI 애니메이션 적용
    Former-commit-id: c90c532c2badf3184cb594098f0a50c359fdfcbf

--- android/app/src/main/java/com/ssafy/firstproject/ui/checkresultdetail/adapter/CheckResultDetailAdapter.kt
+++ android/app/src/main/java/com/ssafy/firstproject/ui/checkresultdetail/adapter/CheckResultDetailAdapter.kt
@@ -1,12 +1,14 @@
 package com.ssafy.firstproject.ui.checkresultdetail.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.ssafy.firstproject.data.model.NewsContent
 import com.ssafy.firstproject.databinding.ItemCheckResultDetailBinding
 import com.ssafy.firstproject.util.ViewAnimationUtil.animateProgress
+import com.ssafy.firstproject.util.ViewAnimationUtil.rotateImage
 
 class CheckResultDetailAdapter :
     ListAdapter<NewsContent, CheckResultDetailAdapter.CheckResultDetailViewHolder>(
@@ -15,13 +17,29 @@ class CheckResultDetailAdapter :
     inner class CheckResultDetailViewHolder(private val binding: ItemCheckResultDetailBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
+        private var currentRotation = 0f // 현재 회전 각도 상태 저장
+
         fun bind(newsContent: NewsContent) {
             binding.tvNewsItemContent.text = newsContent.content
-            binding.tvResult.text = newsContent.result
+//            binding.tvResult.text = newsContent.result
+
+            val cleanText = binding.tvItemResultDetail.text.toString().replace(" ", "\u00A0")
+            binding.tvItemResultDetail.text = cleanText
 
             val randomProgress = (1..100).random()
 
             animateProgress(binding.pbNewsParagraphTruth, randomProgress)
+
+            binding.tvItemPercent.text = "${randomProgress}%"
+
+            binding.root.setOnClickListener {
+                currentRotation += if (currentRotation % 180 == 0f) 90f else -90f
+                rotateImage(binding.ivItem, currentRotation)
+
+                binding.tvItemResultDetail.apply {
+                    visibility = if (visibility == View.VISIBLE) View.GONE else View.VISIBLE
+                }
+            }
         }
     }
 
