b4260a7d2a38d97a80c5967fe37b8b24dcae20ee
민수현 <minsh2344@gmail.com>
2025-02-13 14:08:57 +0900

    Feat: AccessToken SharedPreference에 구현
    Former-commit-id: be7e767eceb6ecc8028c8077a0f63aeb0fd5bfa9

--- android/app/src/main/java/com/ssafy/firstproject/data/source/local/SharedPreferencesUtil.kt
+++ android/app/src/main/java/com/ssafy/firstproject/data/source/local/SharedPreferencesUtil.kt
@@ -7,19 +7,19 @@ class SharedPreferencesUtil(context: Context) {
 
     companion object {
         private const val SHARED_PREFERENCES_NAME = "shared_preferences"
-        private const val COOKIES_KEY_NAME = "cookies"
+        private const val ACCESS_TOKEN_KEY_NAME = "access_token"
     }
 
     private var preferences: SharedPreferences =
         context.getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
 
-    fun addUserCookie(cookies: HashSet<String>) {
+    fun addAccessToken(accessToken: String) {
         val editor = preferences.edit()
-        editor.putStringSet(COOKIES_KEY_NAME, cookies)
+        editor.putString(ACCESS_TOKEN_KEY_NAME, accessToken)
         editor.apply()
     }
 
-    fun getUserCookie(): MutableSet<String>? {
-        return preferences.getStringSet(COOKIES_KEY_NAME, HashSet())
+    fun getAccessToken(): String? {
+        return preferences.getString(ACCESS_TOKEN_KEY_NAME, "")
     }
 }
