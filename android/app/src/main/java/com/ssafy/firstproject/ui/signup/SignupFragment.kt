b660bac6c93bed605c65b2e273b155a9ce56b689
민수현 <minsh2344@gmail.com>
2025-02-14 14:12:25 +0900

    Feat: 회원가입 예외처리 구현
    Former-commit-id: 6c05e73418baf13d159bdcf943143042fbf848c7

--- android/app/src/main/java/com/ssafy/firstproject/ui/signup/SignupFragment.kt
+++ android/app/src/main/java/com/ssafy/firstproject/ui/signup/SignupFragment.kt
@@ -14,6 +14,8 @@ import com.ssafy.firstproject.databinding.FragmentSignupBinding
 import com.ssafy.firstproject.ui.signup.viewmodel.SignupViewModel
 import com.ssafy.firstproject.util.setOnSingleClickListener
 
+private const val TAG = "SignupFragment"
+
 class SignupFragment : BaseFragment<FragmentSignupBinding>(
     FragmentSignupBinding::bind,
     R.layout.fragment_signup
@@ -46,12 +48,10 @@ class SignupFragment : BaseFragment<FragmentSignupBinding>(
 
         binding.tieSignupIdInput.addTextChangedListener {
             viewModel.checkIdValidation(it.toString())
-            viewModel.checkAllValidation()
         }
 
         binding.tieSignupPwInput.addTextChangedListener {
             viewModel.checkPasswordValidation(it.toString())
-            viewModel.checkAllValidation()
         }
 
         binding.tieSignupPwCheckInput.addTextChangedListener {
@@ -59,60 +59,123 @@ class SignupFragment : BaseFragment<FragmentSignupBinding>(
                 binding.tieSignupPwInput.text.toString(),
                 it.toString()
             )
-            viewModel.checkAllValidation()
         }
 
         binding.tieSignupNicknameInput.addTextChangedListener {
             viewModel.checkNickNameValidation(it.toString())
-            viewModel.checkAllValidation()
+        }
+
+        binding.btnCheckId.setOnSingleClickListener {
+            viewModel.checkIdDuplicate(binding.tieSignupIdInput.text.toString())
+        }
+
+        binding.btnCheckNickname.setOnSingleClickListener {
+            viewModel.checkNicknameDuplicate(binding.tieSignupNicknameInput.text.toString())
         }
     }
 
     private fun observeValidation() {
 
         viewModel.isIdValid.observe(viewLifecycleOwner) {
-            if (!it) {
+            if (it) {
+                binding.tvIdInputMetaInfo.visibility = View.GONE
+            } else {
                 binding.tvIdInputMetaInfo.visibility = View.VISIBLE
                 binding.tvIdInputMetaInfo.text = getString(R.string.user_limit_message)
-            } else {
-                binding.tvIdInputMetaInfo.visibility = View.GONE
+                binding.tvIdInputMetaInfo.setTextColor(
+                    getColor(
+                        requireContext(),
+                        R.color.maximumRed
+                    )
+                )
             }
+
+            binding.btnCheckId.isEnabled = it
+            viewModel.checkAllValidation()
         }
 
         viewModel.isPasswordValid.observe(viewLifecycleOwner) {
-            if (!it) {
+            if (it) {
+                binding.tvPwMetaInfo.visibility = View.GONE
+                viewModel.checkPasswordMatch(
+                    binding.tieSignupPwInput.text.toString(),
+                    binding.tieSignupPwCheckInput.text.toString()
+                )
+            } else {
                 binding.tvPwMetaInfo.visibility = View.VISIBLE
                 binding.tvPwMetaInfo.text = getString(R.string.user_limit_message)
-            } else {
-                binding.tvPwMetaInfo.visibility = View.GONE
+                binding.tvPwMetaInfo.setTextColor(getColor(requireContext(), R.color.maximumRed))
             }
+            viewModel.checkAllValidation()
         }
 
         viewModel.isPasswordMatch.observe(viewLifecycleOwner) {
-            if (!it) {
-                binding.tvPwMetaInfo.visibility = View.VISIBLE
-                binding.tvPwMetaInfo.text = getString(R.string.user_not_correct_message)
-                binding.tvPwMetaInfo.setTextColor(getColor(requireContext(), R.color.maximumRed))
-            } else {
+            if (it) {
                 if (viewModel.isPasswordValid.value == true) {
+                    binding.tvPwMetaInfo.visibility = View.VISIBLE
                     binding.tvPwMetaInfo.text = getString(R.string.user_password_message)
                     binding.tvPwMetaInfo.setTextColor(Color.BLUE)
                 }
+            } else {
+                binding.tvPwMetaInfo.visibility = View.VISIBLE
+                binding.tvPwMetaInfo.text = getString(R.string.user_not_correct_message)
+                binding.tvPwMetaInfo.setTextColor(getColor(requireContext(), R.color.maximumRed))
             }
+            viewModel.checkAllValidation()
         }
 
         viewModel.isNicknameValid.observe(viewLifecycleOwner) {
-            if (!it) {
+            if (it) {
+                binding.tvNicknameMetaInfo.visibility = View.GONE
+            } else {
                 binding.tvNicknameMetaInfo.visibility = View.VISIBLE
                 binding.tvNicknameMetaInfo.text = getString(R.string.nickname_limit_message)
-            } else {
-                binding.tvNicknameMetaInfo.visibility = View.GONE
             }
+
+            binding.btnCheckNickname.isEnabled = it
+            viewModel.checkAllValidation()
+        }
+
+        viewModel.isIdDuplicate.observe(viewLifecycleOwner) {
+            if (it.available) {
+                binding.tvIdInputMetaInfo.visibility = View.VISIBLE
+                binding.tvIdInputMetaInfo.text = it.message
+                binding.tvIdInputMetaInfo.setTextColor(Color.BLUE)
+            } else {
+                binding.tvIdInputMetaInfo.visibility = View.VISIBLE
+                binding.tvIdInputMetaInfo.text = it.message
+                binding.tvIdInputMetaInfo.setTextColor(
+                    getColor(
+                        requireContext(),
+                        R.color.maximumRed
+                    )
+                )
+            }
+            viewModel.checkAllValidation()
+        }
+
+        viewModel.isNicknameDuplicate.observe(viewLifecycleOwner) {
+            if (it.available) {
+                binding.tvNicknameMetaInfo.visibility = View.VISIBLE
+                binding.tvNicknameMetaInfo.text = it.message
+                binding.tvNicknameMetaInfo.setTextColor(Color.BLUE)
+            } else {
+                binding.tvNicknameMetaInfo.visibility = View.VISIBLE
+                binding.tvNicknameMetaInfo.text = it.message
+                binding.tvNicknameMetaInfo.setTextColor(
+                    getColor(
+                        requireContext(),
+                        R.color.maximumRed
+                    )
+                )
+            }
+            viewModel.checkAllValidation()
         }
 
         viewModel.isAllValid.observe(viewLifecycleOwner) {
             binding.btnSignup.isEnabled = it
         }
+
     }
 
     private fun observeSignUpSuccess() {
--- android/app/src/main/java/com/ssafy/firstproject/ui/signup/viewmodel/SignupViewModel.kt
+++ android/app/src/main/java/com/ssafy/firstproject/ui/signup/viewmodel/SignupViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ssafy.firstproject.base.ApplicationClass
+import com.ssafy.firstproject.data.model.DuplicateCheckResponse
 import com.ssafy.firstproject.data.model.User
 import kotlinx.coroutines.launch
 
@@ -25,8 +26,17 @@ class SignupViewModel : ViewModel() {
     private val _isNicknameValid = MutableLiveData<Boolean>()
     val isNicknameValid: LiveData<Boolean> get() = _isNicknameValid
 
+    private val _isIdDuplicate = MutableLiveData<DuplicateCheckResponse>()
+    val isIdDuplicate: LiveData<DuplicateCheckResponse> get() = _isIdDuplicate
+
+    private val _isNicknameDuplicate = MutableLiveData<DuplicateCheckResponse>()
+    val isNicknameDuplicate: LiveData<DuplicateCheckResponse> get() = _isNicknameDuplicate
+
     private val _isAllValid = MutableLiveData<Boolean>()
     val isAllValid: LiveData<Boolean> get() = _isAllValid
+
+    private val _isSignupSuccess = MutableLiveData<Boolean>()
+    val isSignupSuccess: LiveData<Boolean> get() = _isSignupSuccess
 
     fun checkIdValidation(id: String) {
         _isIdValid.value = id.matches("^[a-z0-9]{4,12}$".toRegex())
@@ -48,13 +58,48 @@ class SignupViewModel : ViewModel() {
         _isAllValid.value = (isIdValid.value == true &&
                 isPasswordValid.value == true &&
                 isPasswordMatch.value == true &&
-                isNicknameValid.value == true)
+                isNicknameValid.value == true &&
+                isIdDuplicate.value?.available == true &&
+                isNicknameDuplicate.value?.available == true)
     }
 
-    // 회원가입 API 연동
-    private val _isSignupSuccess = MutableLiveData<Boolean>()
-    val isSignupSuccess: LiveData<Boolean>
-        get() = _isSignupSuccess
+    fun checkIdDuplicate(id: String) {
+        viewModelScope.launch {
+            kotlin.runCatching {
+                ApplicationClass.userRepository.checkId(id)
+            }.onSuccess { response ->
+                if (response.isSuccessful) {
+                    response.body()?.let {
+                        _isIdDuplicate.value = it
+                    }
+                    Log.d(TAG, "checkIdDuplicate: ${response.body()}")
+                } else {
+                    Log.d(TAG, "checkIdDuplicate fail: ${response.code()}")
+                }
+            }.onFailure {
+                Log.e(TAG, "checkIdDuplicate: ${it.message}", it)
+            }
+        }
+    }
+
+    fun checkNicknameDuplicate(nickname: String) {
+        viewModelScope.launch {
+            kotlin.runCatching {
+                ApplicationClass.userRepository.checkNickname(nickname)
+            }.onSuccess { response ->
+                if (response.isSuccessful) {
+                    response.body()?.let {
+                        _isNicknameDuplicate.value = it
+                    }
+                    Log.d(TAG, "checkNicknameDuplicate: ${response.body()}")
+                } else {
+                    Log.d(TAG, "checkNicknameDuplicate fail: ${response.code()}")
+                }
+            }.onFailure {
+                Log.e(TAG, "checkNicknameDuplicate: ${it.message}", it)
+            }
+        }
+    }
 
     fun signUp(user: User) {
         viewModelScope.launch {
