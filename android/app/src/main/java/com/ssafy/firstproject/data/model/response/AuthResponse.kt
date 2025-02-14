cd02f1f1abb768c0184d8e3f4a3249eba821984b
민수현 <minsh2344@gmail.com>
2025-02-14 17:45:31 +0900

    Feat: AuthResponse dto에 refreshToken 추가
    Former-commit-id: 9b25b8a1dc48ecda38be4e03169d710c5a0277d1

--- android/app/src/main/java/com/ssafy/firstproject/data/model/response/AuthResponse.kt
+++ android/app/src/main/java/com/ssafy/firstproject/data/model/response/AuthResponse.kt
@@ -3,5 +3,6 @@ package com.ssafy.firstproject.data.model.response
 data class AuthResponse(
     val success: Boolean,
     val accessToken: String,
+    val refreshToken: String,
     val error: String
 )
