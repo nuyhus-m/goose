e71f37ebc2e5f334fa1c14659ec2aa2118b11fcb
박장훈 <gretea4489@gmail.com>
2025-02-16 17:17:52 +0900

    Feat: 신뢰도, 편향성 정보 조회 기능 구현
    Former-commit-id: f7e1b8c5c35f0b9a62efee9d19f088c8a31cde2d

--- android/app/src/main/java/com/ssafy/firstproject/ui/newsresult/NewsResultFragment.kt
+++ android/app/src/main/java/com/ssafy/firstproject/ui/newsresult/NewsResultFragment.kt
@@ -1,17 +1,29 @@
 package com.ssafy.firstproject.ui.newsresult
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
+import android.util.Log
 import android.view.View
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.ssafy.firstproject.R
 import com.ssafy.firstproject.base.BaseFragment
 import com.ssafy.firstproject.databinding.FragmentNewsResultBinding
 import com.ssafy.firstproject.util.ViewAnimationUtil.animateProgress
 
+private const val TAG = "NewsResultFragment_ssafy"
 class NewsResultFragment : BaseFragment<FragmentNewsResultBinding>(
     FragmentNewsResultBinding::bind,
     R.layout.fragment_news_result
 ) {
+    private val args: NewsResultFragmentArgs by navArgs()
+
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -25,8 +37,40 @@ class NewsResultFragment : BaseFragment<FragmentNewsResultBinding>(
             findNavController().navigate(R.id.dest_check_result_detail)
         }
 
-        animateProgress(binding.pbTruth, 30)
+        val newsArticle = args.newsArticle
+
+        Log.d(TAG, "onViewCreated: $newsArticle")
+
+        newsArticle.reliability?.let {
+            val trustScore = "${it.toInt()}%"
+            val fullText = "해당 기사의\n신뢰도는\n$trustScore 입니다."
+
+            // 함수 호출
+            setTextWithColoredSubString(binding.tvBubbleTruth, fullText, trustScore, Color.BLUE)
+
+            binding.tvPercentTruth.text = "${it.toInt()}%"
+            animateProgress(binding.pbTruth, it.toInt())
+        }
+        newsArticle.biasScore?.let {
+            binding.tvBiasPercent.text = "${it.toInt()}%"
+            animateProgress(binding.pbBias, it.toInt())
+        }
+
         animateProgress(binding.pbAi, 60)
-        animateProgress(binding.pbBias, 90)
+        binding.tvAiWhetherPercent.text = "60%"
+    }
+
+    private fun setTextWithColoredSubString(textView: TextView, fullText: String, targetSubstring: String, color: Int) {
+        val spannable = SpannableStringBuilder(fullText)
+
+        // targetSubstring 부분의 시작 인덱스 찾기
+        val startIndex = fullText.indexOf(targetSubstring)
+        val endIndex = startIndex + targetSubstring.length
+
+        // 특정 부분만 색상 변경
+        spannable.setSpan(ForegroundColorSpan(color), startIndex, endIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        // TextView에 적용
+        textView.text = spannable
     }
 }
