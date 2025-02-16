d62437ce3fa42abb4791485ea6f2431218d3226c
박장훈 <gretea4489@gmail.com>
2025-02-16 17:52:14 +0900

    Feat: CheckResutDeatilFragment safeargs 받기
    Former-commit-id: 03935d21de9ab21710e961ab3f0da565516294d0

--- android/app/src/main/java/com/ssafy/firstproject/ui/checkresultdetail/CheckResultDetailFragment.kt
+++ android/app/src/main/java/com/ssafy/firstproject/ui/checkresultdetail/CheckResultDetailFragment.kt
@@ -1,8 +1,10 @@
 package com.ssafy.firstproject.ui.checkresultdetail
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ssafy.firstproject.R
 import com.ssafy.firstproject.base.BaseFragment
@@ -11,14 +13,20 @@ import com.ssafy.firstproject.databinding.FragmentCheckResultDetailBinding
 import com.ssafy.firstproject.ui.checkresultdetail.adapter.CheckResultDetailAdapter
 import com.ssafy.firstproject.util.ViewAnimationUtil.animateProgress
 
+private const val TAG = "CheckResultDetailFragment_ssafy"
 class CheckResultDetailFragment : BaseFragment<FragmentCheckResultDetailBinding>(
     FragmentCheckResultDetailBinding::bind,
     R.layout.fragment_check_result_detail
 ) {
+    private val args: CheckResultDetailFragmentArgs by navArgs()
     private lateinit var checkResultDetailAdapter: CheckResultDetailAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val newsArticle = args.newsArticle
+
+        Log.d(TAG, "onViewCreated: $newsArticle")
 
         binding.toolbar.setNavigationOnClickListener {
             findNavController().popBackStack()
