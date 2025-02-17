4750e62316f79077a5b8c134d4cbd32d1c7c4ca8
박장훈 <gretea4489@gmail.com>
2025-02-17 10:37:26 +0900

    Refactor: NewsAnalysisArticle class 프로퍼티 nullable 타입으로 변경
    Former-commit-id: b05aba57530276aa9d12986034406ed28e342d75

--- android/app/src/main/java/com/ssafy/firstproject/data/model/response/NewsAnalysisArticle.kt
+++ android/app/src/main/java/com/ssafy/firstproject/data/model/response/NewsAnalysisArticle.kt
@@ -5,20 +5,20 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class NewsAnalysisArticle(
-    val biasScore: Double?,  // JSON의 실수형 biasScore 반영
-    val content: String?,  // null 가능하도록 변경
+    val biasScore: Double?,
+    val content: String?,
     val description: String?,
-    val extractedAt: List<Int>?,  // JSON의 배열 형식 반영
-    val id: String,
+    val extractedAt: List<Int>?,
+    val id: String = "",  // 기본값 설정
     val naverLink: String?,
-    val newsAgency: String,
-    val originalLink: String,
+    val newsAgency: String = "",
+    val originalLink: String = "",
     val paragraphReasons: List<String?>,
-    val paragraphReliabilities: List<Double?>,  // JSON의 1.0 → Double로 변경
+    val paragraphReliabilities: List<Double?>,
     val paragraphs: List<String?>,
-    val pubDate: String,
-    val pubDateTimestamp: Long?,  // Int → Long으로 변경
-    val reliability: Double?,  // JSON에서 Double 타입으로 들어오므로 수정
-    val title: String,
+    val pubDate: String = "",
+    val pubDateTimestamp: Long?,
+    val reliability: Double?,
+    val title: String = "",
     val topImage: String?
 ) : Parcelable
