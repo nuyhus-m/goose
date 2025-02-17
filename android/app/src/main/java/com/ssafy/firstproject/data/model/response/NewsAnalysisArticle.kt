fea18a814c572836a7efb866002e0e5f90d65a4c
박장훈 <gretea4489@gmail.com>
2025-02-17 15:40:26 +0900

    Feat: NewsArticle default 값 설정
    Former-commit-id: 57f9ff0cca4b2de63284accadef472ea2d5162ff

--- android/app/src/main/java/com/ssafy/firstproject/data/model/response/NewsAnalysisArticle.kt
+++ android/app/src/main/java/com/ssafy/firstproject/data/model/response/NewsAnalysisArticle.kt
@@ -5,20 +5,20 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class NewsAnalysisArticle(
-    val biasScore: Double?,
-    val content: String?,
-    val description: String?,
-    val extractedAt: List<Int>?,
-    val id: String = "",  // 기본값 설정
-    val naverLink: String?,
-    val newsAgency: String = "",
-    val originalLink: String = "",
-    val paragraphReasons: List<String?>,
-    val paragraphReliabilities: List<Double?>,
-    val paragraphs: List<String?>,
-    val pubDate: String = "",
-    val pubDateTimestamp: Long?,
-    val reliability: Double?,
-    val title: String = "",
-    val topImage: String?
+    val id: String? = "",
+    val biasScore: Double? = 0.0,
+    val content: String? = "",
+    val description: String? = "",
+    val extractedAt: List<Int>? = listOf(),
+    val naverLink: String? = "",
+    val newsAgency: String? = "",
+    val originalLink: String? = "",
+    val paragraphReasons: List<String?> = listOf(),
+    val paragraphReliabilities: List<Double?> = listOf(),
+    val paragraphs: List<String?> = listOf(),
+    val pubDate: String? = "",
+    val pubDateTimestamp: Long? = 0,
+    val reliability: Double? = 0.0,
+    val title: String? = "",
+    val topImage: String? = ""
 ) : Parcelable
