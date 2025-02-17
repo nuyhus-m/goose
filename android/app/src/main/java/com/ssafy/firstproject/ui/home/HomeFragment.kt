ccbb0a8198c645de87476703b0195256aaf3b0a5
박장훈 <gretea4489@gmail.com>
2025-02-17 23:56:10 +0900

    Rename: 패키지 이름 변경
    Former-commit-id: 1c956449864753f0854e39eee7fcaef721f9186b

--- android/app/src/main/java/com/ssafy/firstproject/ui/home/HomeFragment.kt
+++ android/app/src/main/java/com/ssafy/firstproject/ui/home/HomeFragment.kt
@@ -7,7 +7,7 @@ import androidx.navigation.fragment.findNavController
 import com.ssafy.firstproject.R
 import com.ssafy.firstproject.base.BaseFragment
 import com.ssafy.firstproject.databinding.FragmentHomeBinding
-import com.ssafy.firstproject.ui.home.adpater.NewsAdapter
+import com.ssafy.firstproject.ui.home.adapter.NewsAdapter
 import com.ssafy.firstproject.ui.home.viewmodel.HomeViewModel
 
 private const val TAG = "HomeFragment"
--- android/app/src/main/java/com/ssafy/firstproject/ui/search/SearchFragment.kt
+++ android/app/src/main/java/com/ssafy/firstproject/ui/search/SearchFragment.kt
@@ -8,7 +8,7 @@ import androidx.navigation.fragment.findNavController
 import com.ssafy.firstproject.R
 import com.ssafy.firstproject.base.BaseFragment
 import com.ssafy.firstproject.databinding.FragmentSearchBinding
-import com.ssafy.firstproject.ui.home.adpater.NewsAdapter
+import com.ssafy.firstproject.ui.home.adapter.NewsAdapter
 import com.ssafy.firstproject.ui.search.viewmodel.SearchViewModel
 
 class SearchFragment : BaseFragment<FragmentSearchBinding>(
