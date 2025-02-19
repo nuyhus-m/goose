6fe1a8061ca1cb6639e269f858c9d43a2c573095
박장훈 <gretea4489@gmail.com>
2025-02-19 11:08:38 +0900

    Feat: Progressbar 신뢰도 마다 리소스 다르게 적용
    Former-commit-id: a88a0821e502bc6377dd53809d09530f19ea752d

--- android/app/src/main/java/com/ssafy/firstproject/util/ViewAnimationUtil.kt
+++ android/app/src/main/java/com/ssafy/firstproject/util/ViewAnimationUtil.kt
@@ -1,16 +1,26 @@
 package com.ssafy.firstproject.util
 
 import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
 import android.view.animation.DecelerateInterpolator
 import android.widget.ImageView
 import android.widget.ProgressBar
+import com.ssafy.firstproject.R
 
 object ViewAnimationUtil {
+    @SuppressLint("UseCompatLoadingForDrawables")
     fun animateProgress(progressBar: ProgressBar, targetProgress: Int) {
         val currentProgress = progressBar.progress
 
         if (targetProgress <= currentProgress) return
 
+        if (targetProgress < 33) {
+            progressBar.progressDrawable = progressBar.context.getDrawable(R.drawable.bg_progress_bar_33)
+        } else if (targetProgress < 66) {
+            progressBar.progressDrawable = progressBar.context.getDrawable(R.drawable.bg_progress_bar_66)
+        } else {
+            progressBar.progressDrawable = progressBar.context.getDrawable(R.drawable.bg_progress_bar_100)
+        }
         // 애니메이션 설정
         val animator = ObjectAnimator.ofInt(progressBar, "progress", currentProgress, targetProgress).apply {
             duration = 500
