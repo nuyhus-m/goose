7e54607e625ce237fae6144ffc180fca253f8da0
박장훈 <gretea4489@gmail.com>
2025-02-19 14:57:26 +0900

    Feat: 패딩 주는 함수 및 dp 변환 확장함수 ViewUtil 추가
    Former-commit-id: b3c4993a63141d7b8ff35461e3b5693b9f2e56e3

--- android/app/src/main/java/com/ssafy/firstproject/util/ViewUtil.kt
+++ android/app/src/main/java/com/ssafy/firstproject/util/ViewUtil.kt
@@ -1,7 +1,11 @@
 package com.ssafy.firstproject.util
 
+import android.util.TypedValue
+import android.view.View
 import android.widget.ProgressBar
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
+import androidx.core.view.updatePadding
 import com.ssafy.firstproject.R
 
 object ViewUtil {
@@ -14,4 +18,25 @@ object ViewUtil {
         }
         progressBar.progressDrawable = ContextCompat.getDrawable(context, drawableRes)
     }
+
+    // dp를 px로 변환하는 확장 함수
+    fun Int.toPx(view: View): Int {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            this.toFloat(),
+            view.resources.displayMetrics
+        ).toInt()
+    }
+
+    fun setConstraintLayoutBackgroundByTarget(constraintLayout: ConstraintLayout, targetProgress: Int) {
+        val context = constraintLayout.context
+
+        val backgroundRes = R.drawable.bg_corn_flower_blue_8dp
+
+        constraintLayout.background = ContextCompat.getDrawable(context, backgroundRes)
+
+        val paddingPx = 12.toPx(constraintLayout)
+        constraintLayout.updatePadding(paddingPx, paddingPx, paddingPx, paddingPx)
+    }
+
 }
